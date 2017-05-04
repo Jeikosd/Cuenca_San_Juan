82f9fdc4fe8db05c03cd8a5d1d88606cd8c4ab89
Jeikosd <jeison.mesa@correounivalle.edu.co>
2017-05-04 16:10:21 -0500

    change snowfall to foreach with doSnow

--- extract_by_mask.R
+++ extract_by_mask.R
@@ -12,7 +12,7 @@ unzip_raster <- function(zipfile, shape) {
   require(tmaptools)
   
   # shape <- cuenca
-  # zipfile <- names_zip[1]
+  # zipfile <- names_zip[i]
   
   
   
@@ -46,30 +46,61 @@ unzip_raster <- function(zipfile, shape) {
 
 
 
-
-
 path <- 'data/'
 
 names_zip <- list.files(paste0(path, 'waterworld_weekly/'), full.names = T)
-cuenca <- st_read(dsn = paste0(path, '/mask/sanJuan1order.shp'), layer = 'sanJuan1order')
-
-library(snowfall)
-sfInit(parallel = TRUE, cpus = 3)
-sfExport('names_zip')
-sfExport('cuenca')
-sfExport('path')
+cuenca <- st_read(dsn = paste0(path, 'mask/cuencas_finalOK.shp'), layer = 'cuencas_finalOK')
 
 
-sfLibrary(raster)
-sfLibrary(tidyverse)
-sfLibrary(sf)
-sfLibrary(tmaptools)
+
+library(foreach)
+library(doSNOW)
+
+
+cl <- makeCluster(3)
+registerDoSNOW(cl)  ## For Windows
+
+
+length_run <- length(names_zip)
+
+# pb <- txtProgressBar(max = length_run, style = 3)
+# progress <- function(n) setTxtProgressBar(pb, n)
+# 
+# opts <- list(progress=progress)
+
+extract_by_mask <- foreach(i = 1:length_run, .packages = c('raster', 'tidyverse', 'sf', 'tmaptools')) %dopar% {
+  
+  
+          unzip_raster(names_zip[i], shape = cuenca) 
+  
+  
+}
+
+
+# close(pb)
+stopCluster(cl)
+
+
+## unzip_raster(names_zip[153], shape = cuenca) 
+## change to foreach
+
+# library(snowfall)
+# sfInit(parallel = TRUE, cpus = 3)
+# sfExport('names_zip')
+# sfExport('cuenca')
+# sfExport('path')
+
+
+# sfLibrary(raster)
+# sfLibrary(tidyverse)
+# sfLibrary(sf)
+# sfLibrary(tmaptools)
 # sfLibrary(snowfall)
-extract_by_mask <- sfLapply(names_zip, unzip_raster, shape = cuenca)
+# extract_by_mask <- sfLapply(names_zip, unzip_raster, shape = cuenca)
 
 # sfClusterMap(writeOGR, obj = shapes_subcuencas, dsn = dir_out, layer = raster_names, driver = "ESRI Shapefile") probar si creando una funcion sirve
-gc()
-sfStop() 
-gc()
+# gc()
+# sfStop() 
+# gc()
 
 
